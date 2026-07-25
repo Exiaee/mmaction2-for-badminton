abf4bae73d69c53f97954af8b3569f392b8a74f2
exiaee <122559930+Exiaee@users.noreply.github.com>
2026-07-26 01:40:03 +0800

    Add files via upload

--- mmaction2/stgcnpp_badminton_3d.py
+++ mmaction2/stgcnpp_badminton_3d.py
@@ -1,1 +1,127 @@
+_base_='stgcnpp_8xb16-joint-u100-80e_ntu60-xsub-keypoint-3d.py'
 
+
+dataset_type='PoseDataset'
+
+#ann_file=r"C:\D\NCTU_CS\Thesis\ST_GCN\3D_Skeleton\badminton_3d_train_28.pkl"
+ann_file=r"C:\D\NCTU_CS\Thesis\ST_GCN\3D_Skeleton\badminton_3d_all.pkl"
+model = dict(
+    backbone=dict(
+        graph_cfg=dict(
+            layout='coco',
+            mode='spatial'
+        )
+    ),
+    cls_head=dict(
+        num_classes=3
+    )
+)
+'''model = dict(
+    type='RecognizerGCN',
+    backbone=dict(
+        type='STGCN',
+        gcn_adaptive='init',
+        gcn_with_res=True,
+        tcn_type='mstcn',
+        graph_cfg=dict(layout='nturgb+d', mode='spatial')),
+    cls_head=dict(type='GCNHead', num_classes=60, in_channels=256))'''
+
+train_pipeline = [
+    dict(type='PreNormalize3D'),
+    dict(type='GenSkeFeat', dataset='nturgb+d', feats=['j']),
+    dict(type='UniformSampleFrames', clip_len=32),
+    dict(type='PoseDecode'),
+    dict(type='FormatGCNInput', num_person=1),
+    dict(type='PackActionInputs')
+]
+val_pipeline = [
+    dict(type='PreNormalize3D'),
+    dict(type='GenSkeFeat', dataset='nturgb+d', feats=['j']),
+    dict(
+        type='UniformSampleFrames', clip_len=32, num_clips=1, test_mode=True),
+    dict(type='PoseDecode'),
+    dict(type='FormatGCNInput', num_person=1),
+    dict(type='PackActionInputs')
+]
+test_pipeline=val_pipeline
+'''test_pipeline = [
+    dict(type='PreNormalize3D'),
+    dict(type='GenSkeFeat', dataset='nturgb+d', feats=['j']),
+    dict(
+        type='UniformSampleFrames', clip_len=100, num_clips=10,
+        test_mode=True),
+    dict(type='PoseDecode'),
+    dict(type='FormatGCNInput', num_person=2),
+    dict(type='PackActionInputs')
+]'''
+
+train_dataloader = dict(
+    _delete_=True,
+    batch_size=4,
+    num_workers=0,
+    persistent_workers=False,
+    sampler=dict(type='DefaultSampler', shuffle=True),
+    dataset=dict(
+        type='RepeatDataset',
+        times=5,
+        dataset=dict(
+            type=dataset_type,
+            ann_file=ann_file,
+            pipeline=train_pipeline,
+            split='train')))
+val_dataloader = dict(
+    _delete_=True,
+    batch_size=4,
+    num_workers=0,
+    persistent_workers=False,
+    sampler=dict(type='DefaultSampler', shuffle=False),
+    dataset=dict(
+        type=dataset_type,
+        ann_file=ann_file,
+        pipeline=val_pipeline,
+        split='train',
+        test_mode=True))
+test_dataloader  = val_dataloader
+'''test_dataloader = dict(
+    delete_=True,
+    batch_size=1,
+    num_workers=0,
+    persistent_workers=False,
+    sampler=dict(type='DefaultSampler', shuffle=False),
+    dataset=dict(
+        type=dataset_type,
+        ann_file=ann_file,
+        pipeline=test_pipeline,
+        split='xsub_val',
+        test_mode=True))'''
+
+val_evaluator = [dict(type='AccMetric')]
+test_evaluator = val_evaluator
+
+train_cfg = dict(
+    type='EpochBasedTrainLoop', max_epochs=50, val_begin=1, val_interval=1)
+val_cfg = dict(type='ValLoop')
+test_cfg = dict(type='TestLoop')
+
+work_dir='./work_dirs/stgcnpp_badminton_3d_all'
+
+param_scheduler = [
+    dict(
+        type='CosineAnnealingLR',
+        eta_min=0,
+        T_max=16,
+        by_epoch=True,
+        convert_to_iter_based=True)
+]
+
+optim_wrapper = dict(
+    optimizer=dict(
+        type='SGD', lr=0.1, momentum=0.9, weight_decay=0.0005, nesterov=True))
+
+default_hooks = dict(checkpoint=dict(interval=1), logger=dict(interval=100))
+
+# Default setting for scaling LR automatically
+#   - `enable` means enable scaling LR automatically
+#       or not by default.
+#   - `base_batch_size` = (8 GPUs) x (16 samples per GPU).
+auto_scale_lr = dict(enable=False, base_batch_size=128)
